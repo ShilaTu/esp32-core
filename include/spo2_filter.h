44d80e9b832d463372c41902a1e3ee12bcd7f396
reporiot <pi.epsilon.gamma@web.de>
2020-02-04 20:16:32 +0100

    ulp:refactor filter macros are functions now

--- include/spo2_filter.h
+++ include/spo2_filter.h
@@ -6,8 +6,6 @@
 
 #define ADC_OFFSET_HALF 2048
 
-#define DC_LOWPASS(fb,adc) ((((126*fb)>>7)+((2*(adc))>>7)))
-#define AC_LOWPASS_NO_OFFSET(fb,adc) ((((115*fb)>>7)+((13*(((adc)-ADC_OFFSET_HALF)))>>7)))
 
 /**
  * spo2_filter() - Applies digital filters to adc sample
@@ -18,6 +16,7 @@
  * additionally using the old values from the previous filtered `sample`.
  */
 void spo2_filter(_spo2_input_sample *sample, _spo2_adc_sample *adc_sample);
+
 
 #endif
 
