10de7daac586ba465cd98a66a84f814f3ccb1b29
drechsler <drechsler@tu-berlin.de>
2020-11-21 11:52:58 +0100

    modified part template for new templating mechanism

---
+++ parts/.template.mk
@@ -0,0 +1,8 @@
+
+TEMPLATING_CHOICES   += part
+TEMPLATING_HELP_part := create a new part
+TEMPLATING_PATH_part += .template/part
+TEMPLATING_VARS_part += VERSION
+
+VERSION      := v1
+HELP_VERSION := Sets the first version
